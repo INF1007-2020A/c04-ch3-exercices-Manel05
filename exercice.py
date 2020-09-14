70c47af63428de06b839330bbaf3102e4c6c1a12
Manel05 <manel.keddam@polymtl.ca>
2020-09-14 23:24:31 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -27,11 +27,11 @@ def to_degrees(angle_rads: float) -> tuple:
 
 
 def to_celsius(temperature: float) -> float:
-    return temperature*1,8+32
+    return 
 
 
 def to_farenheit(temperature: float) -> float:
-    return 1,8
+    return temperature*1,8+32
 
 
 def main() -> None:
