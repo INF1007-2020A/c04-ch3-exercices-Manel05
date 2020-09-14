f5f9048997b70fe29f53c84c8b1d1cd6ccb32d1a
Manel05 <manel.keddam@polymtl.ca>
2020-09-14 22:33:16 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -5,12 +5,12 @@
 import math
 
 def square_root(a: float) -> float:
-    return 0.0
+    return sqrt(a)
 
 
 def square(a: float) -> float:
     print(a*a)
-    return 0.0
+    return a*a
 
 
 def average(a: float, b: float, c: float) -> float:
